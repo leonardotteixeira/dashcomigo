e19040fd188f55b3648e78615265ee378ae35038
leonardoteixeira018-gif <leonardoteixeira018@gmail.com>
2026-03-28 13:49:53 -0300

    fix: Remove cacheStore.clear() that crashes on latest PocketBase SDK

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -5,7 +5,4 @@ const pbURL = import.meta.env.VITE_POCKETBASE_URL || 'http://localhost:8090';
 
 export const pb = new PocketBase(pbURL);
 
-// Enable auto caching
-pb.cacheStore.clear();
-
 export default pb;
