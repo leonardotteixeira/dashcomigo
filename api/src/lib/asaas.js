04eadda94238f651bf923cac92a2ac1ff7bb2645
leonardoteixeira018-gif <leonardoteixeira018@gmail.com>
2026-04-12 17:27:49 -0300

    feat: suporte a sandbox Asaas via ASAAS_SANDBOX=true

--- api/src/lib/asaas.js
+++ api/src/lib/asaas.js
@@ -1,7 +1,12 @@
 const axios = require("axios");
 
+const isSandbox = process.env.ASAAS_SANDBOX === "true";
+const baseURL = isSandbox
+  ? "https://sandbox.asaas.com/api/v3"
+  : "https://www.asaas.com/api/v3";
+
 const asaas = axios.create({
-  baseURL: "https://www.asaas.com/api/v3",
+  baseURL,
   headers: {
     "access_token": process.env.ASAAS_API_KEY,
     "Content-Type": "application/json"
